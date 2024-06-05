efb6d9fe1e9ac16c94a94ba7a9180b05a3ec24d0
sampada109 <sampadakaushik2518@gmail.com>
2024-06-05 15:09:18 +0530

    3rd commit - making another route and route function

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
+@app.route('/user/signup')
+def user_signup():
+    return "This is user sign-up page"
+
 
 if __name__ == "__main__":
     app.run(debug = True)
