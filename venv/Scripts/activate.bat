866ec6528c1f7ab44f745f8e85a1e7046b3d1c43
sampada109 <sampadakaushik2518@gmail.com>
2024-06-24 15:50:53 +0530

    deleting previous connections and start it off again

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sampa\Documents\SAMPADA\samp_notes\Projects\REST_API_FLASK\Project_1\venv
+set VIRTUAL_ENV=C:\Users\sampa\Documents\SAMPADA\samp_notes\Projects\REST_API_FLASK\API-2 (with sql-db)\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
