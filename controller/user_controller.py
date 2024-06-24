764936cce4fa7bf70cc6213a611079a9e8359b62
sampada109 <sampadakaushik2518@gmail.com>
2024-06-24 16:33:47 +0530

    9th commit - define getall method to fetch all the users and also imported DictCursor, to get the fetched data in dictionary format

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -2,3 +2,7 @@ from app import app
 from model.user_model import UserModel
 
 obj = UserModel()
+
+@app.route('/user/getall')
+def user_getall():
+    return obj.user_getall_model()
--- model/user_model.py
+++ model/user_model.py
@@ -1,4 +1,6 @@
 import pymysql
+from pymysql.cursors import DictCursor
+import json
 
 class UserModel():
     def __init__(self):
@@ -7,9 +9,19 @@ class UserModel():
                 host="localhost",
                 user="root",
                 password="",
-                database="learning_rest_api"
+                database="learning_rest_api",
+                cursorclass=DictCursor
             )
             self.cursor = self.conn.cursor()
             print('successfully connected!')
         except Exception as e:
             print(e)
+
+    # get all users
+    def user_getall_model(self):
+        self.cursor.execute("SELECT * FROM users")
+        result = self.cursor.fetchall()
+        if len(result)>0:
+            return json.dumps(result)
+        else:
+            return "No Data Exist"
